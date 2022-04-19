2ff0587f6b7f2e1374954286aca826dd0bcd4519
Markus Lim <markuslim24@gmail.com>
2022-04-19 15:10:27 +0800

    add muitheme

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ThemeProvider } from "@mui/material/styles";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return;
+  <ThemeProvider theme={theme}>
+    <Component {...pageProps} />
+  </ThemeProvider>;
 }
 
-export default MyApp
+export default MyApp;
---
+++ styles/muiTheme.js
@@ -0,0 +1,9 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: red[500],
+    },
+  },
+});
